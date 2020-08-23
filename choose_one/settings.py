2464c822e798fa938b3bb6d49a3f66ab2358473e
yurii <jurakulek@gmail.com>
2020-08-23 19:53:08 +0300

    Removed path to env file from env variable

--- choose_one/settings.py
+++ choose_one/settings.py
@@ -15,7 +15,7 @@ import os
 from environs import Env
 
 env = Env()
-env.read_env('.env/vars')
+env.read_env()
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
